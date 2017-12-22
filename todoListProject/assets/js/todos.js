483f0e276191db185497ec959c5d5d2c9ec69081
David Caldwell <ddcaldwell87@gmail.com>
2017-12-21 20:44:38 -0500

    finished jQuery logic on todo list

--- todoListProject/assets/js/todos.js
+++ todoListProject/assets/js/todos.js
@@ -1,4 +1,20 @@
-// check off todos by clicking
-$("li").click(function(){
+// mark todos complete by clicking
+$("ul").on("click", "li", function(){
   $(this).toggleClass("complete");
+});
+
+// clicking the X will delete the todo
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+// add todo when enter is pushed inside input
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    $("ul").append("<li><span>X</span> " + $(this).val() + "</li>");
+    $(this).val("");
+  }
 });

705dbb7f6a27ba31a34932c7a2445f710321af0e
David Caldwell <ddcaldwell87@gmail.com>
2017-12-22 18:18:29 -0500

    finsihed front end of todo list

--- todoListProject/assets/js/todos.js
+++ todoListProject/assets/js/todos.js
@@ -14,7 +14,12 @@ $("ul").on("click", "span", function(event){
 // add todo when enter is pushed inside input
 $("input[type='text']").keypress(function(event){
   if(event.which === 13){
-    $("ul").append("<li><span>X</span> " + $(this).val() + "</li>");
+    $("ul").append("<li><span><i class='fa fa-trash-o'></i></span> " + $(this).val() + "</li>");
     $(this).val("");
   }
+});
+
+// hide and display the input when the plus symbol is clicked
+$(".fa-plus-circle").click(function(){
+  $("input[type='text']").fadeToggle();
 });
